9a1b771c4dbcd0b3c1cf472979fcf377f67820dc
kurenkov004 <mary.kurenkov@gmail.com>
2023-08-30 18:36:49 -0400

    changed vanilla JS to jQuery

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+function newItem(){
+ 
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    let list = $('#list');
+    list.append(li)
+  }
+  
+
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+  
+  
+  function deleteListItem(){
+    li.addClass('delete');
+  }
+
+  crossOutButton.on('click', function deleteListItem(){
+    li.addClass('delete');
+  });
+
+   $('#list').sortable();
+
+}
+
+ 
+
+
+
